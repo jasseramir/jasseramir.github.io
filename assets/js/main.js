d821995cf48343190958c3bac6381e267c1bede9
jasseramir <jasser.amir.work@gmail.com>
2026-06-02 23:39:56 +0300

    add scrollbar and selection styles & add scrollup

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,6 +118,11 @@ contactForm.addEventListener('submit', (e) => {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollup = document.getElementById('scroll-up');
+
+window.addEventListener('scroll', function() {
+    this.scrollY >= 350 ? scrollup.classList.add('show-scroll') : scrollup.classList.remove('show-scroll');
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]');
