f491c62b2a97b7c385885e5e9d211be807e46189
jasseramir <jasser.amir.work@gmail.com>
2026-06-02 21:33:19 +0300

    confirm patch

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,38 +85,38 @@ const contactBtn = document.getElementById('contact-button');
 let timeId = null;
 
 async function sendEmail() {
-  if (timeId) clearTimeout(timeId);
+    if (timeId) clearTimeout(timeId);
 
-  try {
-    contactBtn.disabled = true;
-    contactMessage.style.display = 'block';
-    contactMessage.textContent = 'Sending...';
+    try {
+        contactBtn.disabled = true;
+        contactMessage.style.display = 'block';
+        contactMessage.textContent = 'Sending...';
 
-    const email = await emailjs.sendForm(
-      'service_3pl6sg8',
-      'template_s26dkmr',
-      contactForm,
-      '8-3MlNU4tu0G6NJrt',
-    );
+        const templateParams = {
+            from_name: contactForm.querySelector('input[type="text"]').value,
+            user_email: contactForm.querySelector('input[type="email"]').value,
+            message: contactForm.querySelector('textarea').value
+        };
 
-    contactMessage.textContent = 'Sent successfully';
+        const email = await emailjs.send('service_3pl6sg8', 'template_s26dkmr', templateParams, '8-3MlNU4tu0G6NJrt');
 
-    contactForm.reset();
-  } catch (err) {
-    console.log('failed', err);
-    contactMessage.textContent = JSON.stringify(err);
-  } finally {
-    contactBtn.disabled = false;
+        contactMessage.textContent = 'Sent successfully';
+        contactForm.reset();
 
-    timeId = setTimeout(() => {
-      contactMessage.style.display = 'none';
-    }, 3000);
-  }
+    } catch(err) {
+        console.log(err);
+        contactMessage.textContent = 'Error: ' + (err.text || err.message || JSON.stringify(err) || 'Unknown Error');
+    } finally {
+        contactBtn.disabled = false;
+        timeId = setTimeout(() => {
+            contactMessage.style.display = 'none';
+        }, 3000);
+    }
 }
 
-contactForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  sendEmail();
+contactForm.addEventListener('submit', e => {
+    e.preventDefault();
+    sendEmail();
 });
 
 /*=============== SHOW SCROLL UP ===============*/
